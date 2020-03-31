4394fa3bb6266a1f0bb676615e51ab0f77ff7ce2
Darren Rambaud <xyzst@users.noreply.github.com>
2020-03-31 02:20:29 -0500

    Cleaning up
    - Add another unit test
    - Commit application runner/entry point

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // go has a built in testing framework, each test must be formatted as such: "TestSomeDescriptionHere(t *testing.T)"
 func TestNewDeckShouldReturnExactly52Cards(t *testing.T) {
@@ -9,4 +12,30 @@ func TestNewDeckShouldReturnExactly52Cards(t *testing.T) {
 	if len(deck) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(deck))
 	}
+}
+
+func TestSerializationToFileAndDeserializationToDeck(t *testing.T) {
+	// Remove any remnants from previous tests or test run
+	os.Remove("_myDeck")
+
+	deck := create()
+
+	err := deck.serialize("_myDeck")
+
+	if err != nil {
+		t.Errorf("Unable to serialize deck due to %v", err)
+	}
+
+	saved, err := deserialize("_myDeck")
+
+	if err != nil {
+		t.Errorf("Unable to deserialize byte slice to deck object due to %v", err)
+	}
+
+	if len(saved) != 52 {
+		t.Errorf("Expected 52 cards to be deserialized, but got %v", len(saved))
+	}
+
+	// Clean up
+	os.Remove("_myDeck")
 }
